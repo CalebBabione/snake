ed112d5496d4d23dc817e90c457ecf966dfd8f7d
CalebBabione <caleb.b@tutanota.com>
2025-03-30 14:52:32 -0500

    added controls

--- snake.py
+++ snake.py
@@ -5,17 +5,27 @@ import keyboard
 board = [20, 20]
 body = [[10, 10],[10, 9],[10,8]]
 length = 3
-vector = [1, "up"]
+vector = [1, "right"]
 
 def isBody(x,y):
     for i in range(length):
         if body[i][0] == x and body[i][1] == y:
             return True
 def motion():
-    body.append([body[length - 1][0], body[length - 1][1] - 1]) 
-    body.remove(body[0])
+    if vector[1] == "up":
+        body.append([body[length - 1][0], body[length - 1][1] - 1])
+        body.remove(body[0])
+    if vector[1] == "down":
+        body.append([body[length - 1][0], body[length - 1][1] + 1])
+        body.remove(body[0])
+    if vector[1] == "left":
+        body.append([body[length - 1][0] - 1, body[length - 1][1]])
+        body.remove(body[0])
+    if vector[1] == "right":
+        body.append([body[length - 1][0] + 1, body[length - 1][1]])
+        body.remove(body[0])
 while True:
-    os.system('clear')
+    os.system('cls' if os.name == 'nt' else 'clear')
     for verti in range(board[1]):
         for hori in range(board[0]):
             if verti == 0 or verti == 19:
@@ -30,5 +40,19 @@ while True:
                         print(".", end="")
             if hori == 19:
                 print("")
+    if keyboard.is_pressed('w'):
+        if keyboard.is_pressed:
+            vector[1] = "up"
+    if keyboard.is_pressed('s'):
+        if keyboard.is_pressed:
+            vector[1] = "down"
+    if keyboard.is_pressed('a'):
+        if keyboard.is_pressed:
+            vector[1] = "left"
+    if keyboard.is_pressed('d'):
+        if keyboard.is_pressed:
+            vector[1] = "right"
+        else:
+            continue
     motion()
-    time.sleep(.75)
+    time.sleep(.5)
